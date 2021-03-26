0ef53ac7db4add292ca0703d054e442b6955e326
MiseraDo <miserado@gmail.com>
2021-03-26 08:31:40 +0100

    Add class View

---
+++ app/core/View.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\core;
+
+class View
+{
+    private $route, $path;
+    public $layout = 'default';
+
+    public function __construct($route)
+    {
+        $this->route = $route;
+        $this->path = $route['controller'] . '/' . $route['action'];
+    }
+
+    public function renderView(array $vars)
+    {
+        $path = 'app/views/' . $this->path . '.php';
+        if (file_exists($path)) {
+            extract($vars);
+            ob_start();
+            require_once($path);
+            $content = ob_get_clean();
+            require_once('app/views/layouts/' . $this->layout . '.php');
+        } else self::redirectToErrorPage(404);
+    }
+
+    public function redirectTo(string $url)
+    {
+        header('location: ' . $url);
+        die();
+    }
+
+    static function redirectToErrorPage(string $page)
+    {
+        http_response_code($page);
+        $path = 'app/views/errors/' . $page . '.php';
+        if (file_exists($path))
+            require_once($path);
+        else die(__METHOD__);
+    }
+}
