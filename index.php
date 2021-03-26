4c0dd85bd667974c983a73c872621d10b3a811af
MiseraDo <miserado@gmail.com>
2021-03-26 08:34:43 +0100

    Implementation index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+
+use app\core\Config;
+use app\core\Router;
+
+spl_autoload_register(function (string $ClassName) {
+    $path = str_replace('\\', '/', $ClassName) . '.php';
+    if (file_exists($path))
+        require_once($path);
+});
+
+$CONFIG = new Config();
+$ROUTER = new Router();
+$ROUTER->routeLaunch();
